b935136ec9bcf994c342bbf14afd62380b9e97c3
Kevin Bonds <kevin.bonds@gmail.com>
2024-04-10 14:16:16 -0500

    Section to get the api summary into the modal

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,7 +8,9 @@ import RecipeModal from "./components/RecipeModal";
 const App = () => {
   const [searchTerm, setSearchTerm] = useState<string>("");
   const [recipes, setRecipes] = useState<Recipe[]>([]);
-  const [selectedRecipe, setSelectedRecipe] = useState<Recipe | undefined>(undefined);
+  const [selectedRecipe, setSelectedRecipe] = useState<Recipe | undefined>(
+    undefined
+  );
   const pageNumber = useRef(1);
 
   const handleSearchSubmit = async (event: FormEvent) => {
@@ -17,7 +19,6 @@ const App = () => {
       const recipes = await api.searchRecipes(searchTerm, 1);
       setRecipes(recipes.results);
       pageNumber.current = 1;
-
     } catch (e) {
       console.error(e);
     }
@@ -37,18 +38,30 @@ const App = () => {
   return (
     <div>
       <form onSubmit={(event) => handleSearchSubmit(event)}>
-        <input type="text" required name="" id="" placeholder="Enter a search term" value={searchTerm} onChange={(event) => setSearchTerm(event.target.value)} />
-      <button type="submit">Submit</button>
+        <input
+          type="text"
+          required
+          name=""
+          id=""
+          placeholder="Enter a search term"
+          value={searchTerm}
+          onChange={(event) => setSearchTerm(event.target.value)}
+        />
+        <button type="submit">Submit</button>
       </form>
       {recipes.map((recipe) => (
-          <RecipeCard recipe={recipe} onClick={() => setSelectedRecipe(recipe) }/>
-    ))}
-    
-    <button className="view-more-button" onClick={handleViewMoreClick} >View More</button>
+        <RecipeCard recipe={recipe} onClick={() => setSelectedRecipe(recipe)} />
+      ))}
 
-    {selectedRecipe ? <RecipeModal/> : null}
+      <button className="view-more-button" onClick={handleViewMoreClick}>
+        View More
+      </button>
+
+      {selectedRecipe ? (
+        <RecipeModal recipeId={selectedRecipe.id.toString()} />
+      ) : null}
     </div>
-);
+  );
 };
 
 export default App;
--- frontend/src/components/RecipeModal.tsx
+++ frontend/src/components/RecipeModal.tsx
@@ -1,14 +1,38 @@
-const RecipeModal = () => {
+import { useEffect, useState } from 'react';
+import { RecipeSummary } from '../types';
+import * as RecipeAPI from '../api';
+
+interface Props {
+    recipeId: string;
+}
+
+const RecipeModal = ({recipeId}: Props) => {
+    const [recipeSummary, setRecipeSummary] = useState<RecipeSummary>();
+    useEffect(() =>{
+        const fetchRecipeSummary = async () => {
+            try {
+                const  summaryRecipe = await  RecipeAPI.getRecipeSummary(recipeId);
+                setRecipeSummary(summaryRecipe);
+            } catch (error) {
+                console.log(error);
+            }
+        };
+
+        fetchRecipeSummary();
+    }, [recipeId]);
+    if(!recipeSummary) {
+        return <></>
+    }
   return (
     <>
       <div className="overlay"></div>
       <div className="modal">
         <div className="modal-content">
           <div className="modal-header">
-            <h2>Recipe Title</h2>
+            <h2>{recipeSummary?.id}</h2>
             <span className="close-btn">&times;</span>
           </div>
-          <p>Recipe Summary</p>
+          <p dangerouslySetInnerHTML={{ __html: recipeSummary.summary}}></p>
         </div>
       </div>
     </>
