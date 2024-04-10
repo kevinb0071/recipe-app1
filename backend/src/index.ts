fc4c276488b48ea2d4dc4f5032f839fd811b802d
Kevin Bonds <kevin.bonds@gmail.com>
2024-04-10 13:00:11 -0500

    added summary function to the api

---
+++ backend/src/index.ts
@@ -0,0 +1,32 @@
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import * as RecipeAPI from "./recipe-api";
+
+const app = express();
+app.use(express.json());
+
+app.use(cors());
+
+app.get("/api/recipes/search", async (req, res) => {
+    const searchTerm = req.query.searchTerm as string;
+    const page = parseInt(req.query.page as string);
+    const results = await RecipeAPI.searchRecipes(searchTerm, page);
+  
+    return res.json(results);
+  });
+
+app.get("/api/recipes/:recipeId/summary", async (req, res) => {
+  const recipeId = req.params.recipeId;
+  const results = await RecipeAPI.getRecipeSummary(recipeId);
+  return res.json(results);
+});
+
+
+
+
+
+
+app.listen(5000, () => {
+    console.log("Server running on localhost");
+});
---
+++ frontend/src/App.tsx
@@ -0,0 +1,50 @@
+import "./App.css";
+import { FormEvent, useRef, useState } from "react";
+import * as api from "./api";
+import { Recipe } from "./types";
+import RecipeCard from "./components/RecipeCard";
+
+const App = () => {
+  const [searchTerm, setSearchTerm] = useState<string>("");
+  const [recipes, setRecipes] = useState<Recipe[]>([]);
+  const pageNumber = useRef(1);
+
+  const handleSearchSubmit = async (event: FormEvent) => {
+    event.preventDefault();
+    try {
+      const recipes = await api.searchRecipes(searchTerm, 1);
+      setRecipes(recipes.results);
+      pageNumber.current = 1;
+
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  const handleViewMoreClick = async () => {
+    const nextPage = pageNumber.current + 1;
+    try {
+      const nextRecipes = await api.searchRecipes(searchTerm, nextPage);
+      setRecipes([...recipes, ...nextRecipes.results]);
+      pageNumber.current = nextPage;
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <div>
+      <form onSubmit={(event) => handleSearchSubmit(event)}>
+        <input type="text" required name="" id="" placeholder="Enter a search term" value={searchTerm} onChange={(event) => setSearchTerm(event.target.value)} />
+      <button type="submit">Submit</button>
+      </form>
+      {recipes.map((recipe) => (
+          <RecipeCard recipe={recipe} />
+    ))}
+    
+    <button className="view-more-button" onClick={handleViewMoreClick} >View More</button>
+    </div>
+);
+};
+
+export default App;
